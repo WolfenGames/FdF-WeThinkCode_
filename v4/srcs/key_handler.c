afc8588180e5169c69a70a8185c0b398bf9fc87c
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-06-11 07:28:18 +0200

    Version 4

---
+++ v4/srcs/key_handler.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keyHandle.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwolf <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/07 08:35:12 by jwolf             #+#    #+#             */
+/*   Updated: 2018/06/08 11:58:57 by jwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+
+static int		get_displacement_x(int keycode)
+{
+	if (keycode == ARROW_LEFT)
+		return (-1);
+	if (keycode == ARROW_RIGHT)
+		return (1);
+	return (0);
+}
+
+static int		get_displacement_y(int keycode)
+{
+	if (keycode == ARROW_UP)
+		return (-1);
+	if (keycode == ARROW_DOWN)
+		return (1);
+	return (0);
+}
+
+static	int		get_scale(int keycode, t_map *map)
+{
+	if (keycode == PG_DOWN && map->scale >= 2)
+		return (-1);
+	if (keycode == PG_UP && map->scale < MAX_SCALE)
+		return (1);
+	return (0);
+}
+
+int				keyhook(int keycode, t_map *map)
+{
+	if (keycode == ESC)
+		escape();
+	window_new(keycode, map);
+	map->map_x += get_displacement_x(keycode) * map->scale;
+	map->map_z += get_displacement_y(keycode) * map->scale;
+	map->scale += get_scale(keycode, map);
+	draw_map(map);
+	return (0);
+}
