985f05054d4a8c19868991fb3601ad560f19e704
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-06-11 17:59:58 +0200

    auto commit

--- v6/srcs/points.c
+++ v6/srcs/points.c
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   loop_handler.c                                     :+:      :+:    :+:   */
+/*   points.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jwolf <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/06/11 08:05:47 by jwolf             #+#    #+#             */
-/*   Updated: 2018/06/11 08:05:48 by jwolf            ###   ########.fr       */
+/*   Created: 2018/06/07 16:10:46 by jwolf             #+#    #+#             */
+/*   Updated: 2018/06/08 11:11:38 by jwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-int     loop_hook(t_map *map)
+t_points		new_point(int x, int y, int z)
 {
-    mlx_key_hook(map->window, keyhook, map);   
-    return (0);
+	t_points	new_point;
+
+	new_point.x = x;
+	new_point.y = y;
+	new_point.z = z;
+	if (y > 0)
+		new_point.col = C_RED;
+	else if (y < 0)
+		new_point.col = C_GREEN;
+	else
+		new_point.col = C_BLUE;
+	return (new_point);
 }
