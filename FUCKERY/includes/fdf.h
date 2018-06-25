573184b67ba630e1fea20ece115f81bf2500ab1b
Julian <jwolf@student.wethinkcode.co.za>
2018-06-25 15:32:49 +0200

    Added Fuckery
    Changed a few minor things that dont matter too much.

--- FUCKERY/includes/fdf.h
+++ FUCKERY/includes/fdf.h
@@ -12,6 +12,9 @@ typedef struct  s_map
     void        *mlx;
     void        *win;
     void        *img;
+    char        *data;
+    int         bpp;
+    int         endian;
     int         w;
     int         h;
 }               t_map;
--- FUCKERY/srcs/main.c
+++ FUCKERY/srcs/main.c
@@ -2,18 +2,33 @@
 
 int     keyhook(int keycode, t_map *map)
 {
-  
     if (keycode == 53)
     {
         mlx_destroy_window(map->mlx, map->win);
-        exit(0);
+        exit (0);
     }
+    
     return (0);
 }
 
 int     expose_hook(t_map *map)
 {
-    ft_putendl(map->mlx);
+    int     x;
+    int     y;
+
+    y = -1;
+    map->img = mlx_new_image(map->mlx, map->w, map->h);
+    map->data = mlx_get_data_addr(map->img, &map->bpp, 10, &map->endian);
+    mlx_put_image_to_window(map->mlx, map->win, map->img, 0, 0);
+    return (0);
+}
+
+int     close_hook(int button, t_map *map)
+{
+    (void)map;
+    (void)button;
+    exit(0);
+    return (0);
 }
 
 int     main(void)
@@ -21,12 +36,15 @@ int     main(void)
     t_map   *map;
     
     map = malloc(sizeof(t_map));
+    map->bpp = 100;
+    map->endian = 12;
     map->mlx = mlx_init();
     map->w = WIN_W;
     map->h = WIN_H;
     map->win = mlx_new_window(map->mlx, map->w, map->h, "FUCKERY");
-    mlx_key_hook(map->win, keyhook, map);
     mlx_expose_hook(map->win, expose_hook, map);
+    mlx_hook(map->win, 17, 0, close_hook, map);
+    mlx_hook(map->win, 2, 0, keyhook, map);
     mlx_loop(map->mlx);
     return (0);
 }
